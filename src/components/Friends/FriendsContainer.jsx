43327ecc94a05cf2eafe503a1c596eb81749411d
Niobium14 <67588654+Niobium14@users.noreply.github.com>
2020-10-26 14:07:08 +0300

    Container component
    Class component "FriendsAPIComponent" was  moved to "FriendsContainer" and JSX layout was meved to "Friends.jsx"

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -6,7 +6,47 @@ import {
   setCurrentPageActionCreator,
   setTotalUsersCountActionCreator,
 } from "../../redux/myRedusers/friends-reduser";
+import React, { Component } from "react";
+import * as axios from "axios";
 import Friends from "./Friends";
+
+class FriendsContainer extends Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  setPage = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <Friends
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        users={this.props.users}
+        unfollow={this.props.unfollow}
+        currentPage={this.props.currentPage}
+        follow={this.props.follow}
+        setPage={this.setPage}
+      />
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
   return {
@@ -36,5 +76,5 @@ let mapDispatchToProps = (dispatch) => {
     },
   };
 };
-const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends);
-export default FriendsContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsContainer);
+
