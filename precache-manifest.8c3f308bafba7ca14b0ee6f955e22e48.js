bc0260d63e78c7b1a1d92f14737596163aa5c2d1
Alex <niobium1441@gmail.com>
2020-11-23 20:29:54 +0200

    Updates

---
+++ precache-manifest.8c3f308bafba7ca14b0ee6f955e22e48.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f0a5ba487aafde7fcf831eb7fd8fb6",
+    "url": "/Warm-chat/index.html"
+  },
+  {
+    "revision": "83a20fca9512a820708f",
+    "url": "/Warm-chat/static/css/main.c9542500.chunk.css"
+  },
+  {
+    "revision": "546e2caf2c25ffdc24c4",
+    "url": "/Warm-chat/static/js/2.83520a6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Warm-chat/static/js/2.83520a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a20fca9512a820708f",
+    "url": "/Warm-chat/static/js/main.b9cd4694.chunk.js"
+  },
+  {
+    "revision": "a9303439fd8c72099232",
+    "url": "/Warm-chat/static/js/runtime-main.e2ba1559.js"
+  },
+  {
+    "revision": "71d16f9ec3bdeb61ca8a40d13620fd5f",
+    "url": "/Warm-chat/static/media/bg_1.71d16f9e.png"
+  },
+  {
+    "revision": "c6fcde8c7c3a3d9ecbce4cec40815a22",
+    "url": "/Warm-chat/static/media/bg_2.c6fcde8c.png"
+  },
+  {
+    "revision": "bdd633a18dd34f73339d609b5b47febb",
+    "url": "/Warm-chat/static/media/bg_3.bdd633a1.png"
+  },
+  {
+    "revision": "6c21f06617dfb7007ecb1c0942f6a9d6",
+    "url": "/Warm-chat/static/media/bg_music.6c21f066.png"
+  },
+  {
+    "revision": "2ec57330cdc51a8768ca51cd31a60233",
+    "url": "/Warm-chat/static/media/friends_bg1.2ec57330.png"
+  },
+  {
+    "revision": "46438351dde0b3073035a8d8f11eabc0",
+    "url": "/Warm-chat/static/media/header_logo.46438351.png"
+  },
+  {
+    "revision": "f50391822909aeeeb9640efbb4144811",
+    "url": "/Warm-chat/static/media/news_bg1.f5039182.png"
+  },
+  {
+    "revision": "b0f7f9fa136b92f47de0b8e30e4a3c0e",
+    "url": "/Warm-chat/static/media/news_bg3.b0f7f9fa.png"
+  },
+  {
+    "revision": "d8b41c227c484462cdf3fa6c47815833",
+    "url": "/Warm-chat/static/media/news_info1.d8b41c22.jpg"
+  },
+  {
+    "revision": "03a5ac3adc10c130d9d9f80f9cad0068",
+    "url": "/Warm-chat/static/media/news_info2.03a5ac3a.jpg"
+  },
+  {
+    "revision": "2b3736d403c1d77e8b3844b9a648e348",
+    "url": "/Warm-chat/static/media/news_info3.2b3736d4.jpg"
+  },
+  {
+    "revision": "1c565e8f17b8040b2e06c4cad99f092a",
+    "url": "/Warm-chat/static/media/person1.1c565e8f.jpg"
+  },
+  {
+    "revision": "f1b783a660f1c7c5a325432ac34911a8",
+    "url": "/Warm-chat/static/media/person2.f1b783a6.jpg"
+  },
+  {
+    "revision": "80d7b9a8300d572e57909c302bf18246",
+    "url": "/Warm-chat/static/media/person3.80d7b9a8.jpg"
+  },
+  {
+    "revision": "52b531353bef1b9ab0d1a9f9f7b2556c",
+    "url": "/Warm-chat/static/media/playlist1.52b53135.png"
+  },
+  {
+    "revision": "f4bc0797657e3059801de402a964bdae",
+    "url": "/Warm-chat/static/media/playlist2.f4bc0797.png"
+  },
+  {
+    "revision": "d48f19ed524e6dcc560c86e888566bf0",
+    "url": "/Warm-chat/static/media/playlist3.d48f19ed.png"
+  },
+  {
+    "revision": "6084a2acdc0240087d039e749e15520c",
+    "url": "/Warm-chat/static/media/playlist4.6084a2ac.png"
+  },
+  {
+    "revision": "6efcdcfec39465e83c6a5fcaf5eb9e90",
+    "url": "/Warm-chat/static/media/playlist5.6efcdcfe.png"
+  },
+  {
+    "revision": "e96d0ff4789a73e1564673d1e91ddd85",
+    "url": "/Warm-chat/static/media/preloader.e96d0ff4.svg"
+  },
+  {
+    "revision": "23cec310335febbd3cb67461621d0cff",
+    "url": "/Warm-chat/static/media/profile_bg.23cec310.jpg"
+  },
+  {
+    "revision": "92e21265a4f6522bf7111505846c0f82",
+    "url": "/Warm-chat/static/media/profile_pic.92e21265.jpg"
+  },
+  {
+    "revision": "a95274a6d60c3d3780790bd86da55ec1",
+    "url": "/Warm-chat/static/media/spotify.a95274a6.png"
+  }
+]);
