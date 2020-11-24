832bc0497bf932dad80be2ffcea4ecb73d11fba4
Alex <niobium1441@gmail.com>
2020-11-24 19:25:48 +0200

    Updated Login form
    Added login area and also password area

--- src/components/Login/LoginForm/LoginForm.jsx
+++ src/components/Login/LoginForm/LoginForm.jsx
@@ -1,34 +1,27 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { reduxForm } from "redux-form";
 import { maxLength, required } from "../../../utils/validators";
 import { CreateField, SingInField } from "../../common/FormValidation/Field";
 import css from "./LoginForm.module.css";
 
 // MAX LENGTH
 let maxLength30 = maxLength(30);
-let maxLength15 = maxLength(15);
+// let maxLength15 = maxLength(15);
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        {CreateField(SingInField, "email", "text", "Email", [
+        {CreateField(null, SingInField, "email", "text", "Email", [
           required,
           maxLength30,
         ])}
       </div>
       <div>
-        {CreateField(SingInField, "password", "text", "password", [
+        {CreateField(null, SingInField, "password", "password", "password", [
           required,
           maxLength30,
         ])}
-        <Field
-          component={SingInField}
-          name={"password"}
-          type="text"
-          placeholder="password"
-          validate={[required, maxLength15]}
-        />
       </div>
       {props.error && <div className={css.error}>{props.error}</div>}
       <div className={css.recall}>
