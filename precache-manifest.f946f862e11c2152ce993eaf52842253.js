6463fb979766b805c59140f1c80e3cb38114eb09
Alex <niobium1441@gmail.com>
2020-12-05 15:30:05 +0200

    Updates

--- precache-manifest.f946f862e11c2152ce993eaf52842253.js
+++ precache-manifest.f946f862e11c2152ce993eaf52842253.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "811d77478f70f802101ca2e729428737",
+    "revision": "74f3b3d118df5c830ccb797b9380d4ec",
     "url": "/Warm-chat/index.html"
   },
   {
-    "revision": "b241535de6b2c14b85c2",
-    "url": "/Warm-chat/static/css/main.ec1aa39c.chunk.css"
+    "revision": "06b1169b65cb4d8d6928",
+    "url": "/Warm-chat/static/css/main.5433a47d.chunk.css"
   },
   {
-    "revision": "b81883ca21803569417c",
-    "url": "/Warm-chat/static/js/2.46129061.chunk.js"
+    "revision": "3d8493db364105cabd2d",
+    "url": "/Warm-chat/static/js/2.5eddf6e7.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Warm-chat/static/js/2.46129061.chunk.js.LICENSE.txt"
+    "url": "/Warm-chat/static/js/2.5eddf6e7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b241535de6b2c14b85c2",
-    "url": "/Warm-chat/static/js/main.824f1170.chunk.js"
+    "revision": "06b1169b65cb4d8d6928",
+    "url": "/Warm-chat/static/js/main.566d707c.chunk.js"
   },
   {
     "revision": "a9303439fd8c72099232",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Warm-chat/precache-manifest.3036dbfdd9be405ab3ce0a939cfc72b1.js"
+  "/Warm-chat/precache-manifest.f946f862e11c2152ce993eaf52842253.js"
 );
 
 self.addEventListener('message', (event) => {
